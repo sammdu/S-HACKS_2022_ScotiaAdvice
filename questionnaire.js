9140a55bc9946316a51202b9074ac6c60f51e002
Samm Du <hmu@sammdu.com>
2022-08-27 12:33:16 -0400

    get started page started

---
+++ questionnaire.js
@@ -0,0 +1,7 @@
+const quesOptionsElem = document.getElementById("quesOptions");
+const quesOneElem = document.getElementById("quesOne");
+
+function startQues() {
+    quesOptionsElem.style.display = "none";
+    quesOneElem.style.display = "flex";
+}
